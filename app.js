ed9f7f41004aac24846dbe510c7ded5327cead3c
Eddi3_As <AQ43820@etsmtl.net>
2025-04-06 17:42:44 -0400

    test auto deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send('Hello World from test app');
+res.send('Test of auto-deploy');
 });
 
 const PORT = process.env.PORT || 5000;
